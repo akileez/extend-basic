3524605732a0832924bdb233d708fb8f991507b1
akileez <git@keeda.net>
2015-04-27 15:22:22 -0400

    update
    — added license year
    — lint via standard
    — added check for hasOwnProperty

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 /*!
  * extend-basic <https://github.com/akileez/extend-basic>
  *
- * Copyright (c) 2014 Keith Williams, contributors.
+ * Copyright (c) 2014-2015 Keith Williams, contributors.
  * Licensed under the MIT license.
  */
 
-'use strict';
+'use strict'
 
 module.exports = function extend (a, b) {
-
-  for(var key in b) {
-    a[key] = b[key];
+  for (var key in b) {
+    if (b.hasOwnProperty(key)) {
+      a[key] = b[key]
+    }
   }
-  return a;
+  return a
 
-};
+}
